46323938ca46096da2767e5119a9eb51b6bf39f3
Nicolas LABROT <nicolas.labrot@victorbuckservices.com>
2015-07-01 14:38:52 +0200

    Add a method to send an array of MimeMessage #4

--- src/main/java/org/nlab/smtp/transport/connection/ClosableSmtpConnection.java
+++ src/main/java/org/nlab/smtp/transport/connection/ClosableSmtpConnection.java
@@ -1,24 +1,74 @@
 package org.nlab.smtp.transport.connection;
 
+import org.nlab.smtp.exception.MailSendException;
+
 import javax.mail.*;
 import javax.mail.event.TransportListener;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by nlabrot on 30/04/15.
  */
 public interface ClosableSmtpConnection extends AutoCloseable {
 
-    void sendMessage(Message msg, Address[] addresses) throws MessagingException;
+    String HEADER_MESSAGE_ID = "Message-ID";
 
+    /**
+     * Send a message to a list of recipients
+     * @param msg
+     * @param recipients
+     * @throws MessagingException
+     * @throws MailSendException
+     */
+    void sendMessage(MimeMessage msg, Address[] recipients) throws MessagingException, MailSendException;
+
+    /**
+     * Send a message. The list of recipients are taken from {@link MimeMessage#getAllRecipients()}
+     * @param msg
+     * @throws MessagingException
+     */
+    void sendMessage(MimeMessage msg) throws MessagingException;
+
+    /**
+     * Send the given array of JavaMail MIME messages in batch. Do not stop the batch when a message could not be sent
+     * {@link MailSendException#getFailedMessages()} will contain the failed messages
+     * @param msgs
+     * @throws MailSendException in case of failure when sending a message
+     */
+    void sendMessages(MimeMessage... msgs) throws MailSendException;
+
+    /**
+     *
+     * @return
+     */
     boolean isConnected();
 
+    /**
+     *
+     * @param l
+     */
     void addTransportListener(TransportListener l);
 
+    /**
+     *
+     * @param l
+     */
     void removeTransportListener(TransportListener l);
 
+    /**
+     *
+     */
     void clearListeners();
 
+    /**
+     *
+     * @return
+     */
     Transport getDelegate();
 
+    /**
+     *
+     * @return
+     */
     Session getSession();
 }
--- src/main/java/org/nlab/smtp/transport/connection/DefaultClosableSmtpConnection.java
+++ src/main/java/org/nlab/smtp/transport/connection/DefaultClosableSmtpConnection.java
@@ -1,12 +1,17 @@
 package org.nlab.smtp.transport.connection;
 
+import org.nlab.smtp.exception.MailSendException;
 import org.nlab.smtp.pool.ObjectPoolAware;
 import org.nlab.smtp.pool.SmtpConnectionPool;
 
 import javax.mail.*;
 import javax.mail.event.TransportListener;
+import javax.mail.internet.MimeMessage;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by nlabrot on 30/04/15.
@@ -22,29 +27,37 @@ public class DefaultClosableSmtpConnection implements ClosableSmtpConnection, Ob
         this.delegate = delegate;
     }
 
-    public void sendMessage(Message msg, Address[] addresses)
-            throws MessagingException{
-        delegate.sendMessage(msg, addresses);
+    public void sendMessage(MimeMessage msg, Address[] recipients) throws MessagingException {
+        doSend(msg, recipients);
     }
 
-    public void addTransportListener(TransportListener l){
+    public void sendMessage(MimeMessage msg) throws MessagingException {
+        doSend(msg, msg.getAllRecipients());
+    }
+
+    public void sendMessages(MimeMessage... msgs) throws MailSendException {
+        doSend(msgs);
+    }
+
+    public void addTransportListener(TransportListener l) {
         transportListeners.add(l);
         delegate.addTransportListener(l);
     }
 
-    public void removeTransportListener(TransportListener l){
+    public void removeTransportListener(TransportListener l) {
         transportListeners.remove(l);
         delegate.removeTransportListener(l);
     }
 
-    public void clearListeners(){
+
+    public void clearListeners() {
         for (TransportListener transportListener : transportListeners) {
             delegate.removeTransportListener(transportListener);
         }
         transportListeners.clear();
     }
 
-    public boolean isConnected(){
+    public boolean isConnected() {
         return delegate.isConnected();
     }
 
@@ -73,4 +86,40 @@ public class DefaultClosableSmtpConnection implements ClosableSmtpConnection, Ob
     public Session getSession() {
         return objectPool.getSession();
     }
+
+
+    private void doSend(MimeMessage mimeMessage, Address[] recipients) throws MessagingException {
+
+        if (mimeMessage.getSentDate() == null) {
+            mimeMessage.setSentDate(new Date());
+        }
+        String messageId = mimeMessage.getMessageID();
+        mimeMessage.saveChanges();
+        if (messageId != null) {
+            // Preserve explicitly specified message id...
+            mimeMessage.setHeader(HEADER_MESSAGE_ID, messageId);
+        }
+        delegate.sendMessage(mimeMessage, recipients);
+    }
+
+
+    private void doSend(MimeMessage... mimeMessages) throws MailSendException {
+        Map<Object, Exception> failedMessages = new LinkedHashMap<>();
+
+        for (int i = 0; i < mimeMessages.length; i++) {
+
+            // Send message via current transport...
+            MimeMessage mimeMessage = mimeMessages[i];
+            try {
+                doSend(mimeMessage, mimeMessage.getAllRecipients());
+            } catch (Exception ex) {
+                failedMessages.put(mimeMessage, ex);
+            }
+        }
+
+        if (!failedMessages.isEmpty()) {
+            throw new MailSendException(failedMessages);
+        }
+    }
+
 }
